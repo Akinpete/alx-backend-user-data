12521c7adf5e514185972f8d86d34de0346a2f4b
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-09-21 19:45:36 +0100

    PEP8 validation

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,11 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-  
+
+
 @app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized_error() -> str:
-  """ GET /api/v1/unauthorized
-    Return:
-      - error 401
-  """
-  abort(401)
+    """ GET /api/v1/unauthorized
+      Return:
+        - error 401
+    """
+    abort(401)
