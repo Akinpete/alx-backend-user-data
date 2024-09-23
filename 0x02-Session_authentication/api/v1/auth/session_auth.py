7a1c7ec2d759862262a68c84c018f3079d0a7c06
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-09-23 21:06:09 +0100

    Generate a Session ID using uuid

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,10 +7,30 @@ from api.v1.auth.auth import Auth
 import base64
 from typing import TypeVar
 from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """
     API Auth via Session Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id
+        Args:
+            user_id (str): The ID of the user for whom to create a session.
+
+        Returns:
+            str: The generated session ID or None if user_id is invalid.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+
+        # Store the session ID with the user_id in the dictionary
+        self.user_id_by_session_id[session_id] = user_id
+
+        # Return the session ID
+        return session_id
