b458fd24dd39e16cf09acfc7cfcafa2491157f3d
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-09-24 14:47:46 +0100

    implement db class

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db")
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email, hashed_password) -> User:
+        """
+        add user instance to db
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        try:
+            self._session.commit()
+        except Exception as e:
+            self._session.rollback()
+            print(f"Error adding user: {e}")
+            return None
+        return user
