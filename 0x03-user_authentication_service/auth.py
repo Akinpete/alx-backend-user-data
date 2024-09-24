3ff3a6fdc6a0155d1c0baa0c33a804778a359bab
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-09-24 16:22:07 +0100

    code check

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,12 @@ import bcrypt
 
 
 def _hash_password(pwd):
+    """
+    Hashes pw using bcrypt
+
+    Args:
+       password
+    """
     bytes = pwd.encode('utf-8')
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
