8354e31bf3c2b54c7d22b88b65d2d69ebfff5fef
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-09-24 11:52:06 +0100

    PEP8

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class User(Base):
     """
     Uaer class model delcaration
@@ -17,7 +18,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=False)
     reset_token = Column(String(250), nullable=False)
-    
-    
-    
-    
