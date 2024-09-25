ebe133ed1d1d5b0ae309729bf1eaefb2d23b11d3
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-09-25 18:27:19 +0100

    POST /reset_password route.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -130,7 +130,6 @@ class Auth:
         reset_token = str(uuid.uuid4())
         try:
             user = self._db.find_user_by(email=email)
-
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
         except NoResultFound:
