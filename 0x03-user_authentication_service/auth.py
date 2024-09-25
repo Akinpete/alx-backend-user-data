3d9c76a6a5bcb998a92812c8843dc3102ed2108d
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-09-25 18:36:06 +0100

    update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -134,3 +134,15 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError("User not found!")
+
+    def update_password(self, reset_token, password):
+        """
+        Update Password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_pwd = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_pwd,
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError("User not found!")
