fb0a8dce27988b72a45105f24f116b3d447b5024
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-09-25 17:01:00 +0100

    find user by session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,3 +91,13 @@ class Auth:
             return session_id
         except Exception as e:
             print(e)
+
+    def get_user_from_session_id(self, session_id):
+        """
+        find user by session ID
+        """
+        try:
+            user = self._db.find_user_by(session_id)
+            return user
+        except NoResultFound:
+            return None
